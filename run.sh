7907c48a87866499f5a2c655be4a9bea50be0ef7
Jhonny Göransson <jhonny.goransson@gmail.com>
2023-01-31 13:57:17 +0100

    It runs now

--- run.sh
+++ run.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-java -Djava.library.path="/Users/jhonny/dev/drift-fx-native-lib/native/build"  -cp target/drift-fx-native-lib-1.0-SNAPSHOT.jar   com.driftfxnative.application.App
+JAVA_EXPORTS='--add-exports javafx.graphics/com.sun.javafx.sg.prism=ALL-UNNAMED
+	--add-exports javafx.graphics/com.sun.javafx.scene=ALL-UNNAMED
+	--add-exports javafx.graphics/com.sun.javafx.util=ALL-UNNAMED
+	--add-exports javafx.graphics/com.sun.javafx.tk=ALL-UNNAMED
+	--add-exports javafx.graphics/com.sun.javafx.geom=ALL-UNNAMED
+	--add-exports javafx.graphics/com.sun.javafx.geom.transform=ALL-UNNAMED
+	--add-exports javafx.graphics/com.sun.javafx.scene.text=ALL-UNNAMED
+	--add-exports javafx.graphics/com.sun.javafx.font=ALL-UNNAMED
+	--add-exports javafx.graphics/com.sun.prism.paint=ALL-UNNAMED
+	--add-opens   javafx.graphics/com.sun.prism=ALL-UNNAMED
+	--add-opens   javafx.graphics/com.sun.prism.es2=ALL-UNNAMED
+	--add-opens   javafx.graphics/com.sun.prism.impl=ALL-UNNAMED'
+
+JAVA_MODULES='javafx.controls,javafx.swing,javafx.graphics,javafx.fxml,javafx.media,javafx.web'
+
+#echo $JAVA_EXPORTS
+
+java --module-path $JAVA_FX --add-modules $JAVA_MODULES $JAVA_EXPORTS -Djava.library.path="/Users/jhonny/dev/drift-fx-native-lib/native/build"  -cp "target/drift-fx-native-lib-1.0-SNAPSHOT.jar:/Users/jhonny/.m2/repository/org/eclipse/fx/org.eclipse.fx.drift/1.0.0/org.eclipse.fx.drift-1.0.0.jar"   com.driftfxnative.application.App
--- src/main/java/com/driftfxnative/application/App.java
+++ src/main/java/com/driftfxnative/application/App.java
@@ -1,6 +1,5 @@
 package com.driftfxnative.application;
 
-/*
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -9,9 +8,8 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import org.eclipse.fx.drift.DriftFXSurface;
-*/
 
-public class App
+public class App extends Application
 {
     static
     {
@@ -20,10 +18,33 @@ public class App
 
     private native void nativeInit();
 
-    public static void main( String[] args )
-    {
-        System.out.println("Hello from java!");
+    private DriftFXSurface surface;
 
-        new App().nativeInit();
+    @Override
+    public void start(Stage stage) {
+        /*
+        final GLProfile profile     = GLProfile.get(GLProfile.GL2);
+        GLCapabilities capabilities = new GLCapabilities(profile);
+        */
+
+        surface = new DriftFXSurface();
+
+        String javaVersion   = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l              = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+
+        StackPane pane = new StackPane(l);
+
+        pane.getChildren().add(surface);
+
+        Scene scene = new Scene(pane, 640, 480);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args)
+    {
+        launch();
     }
 }
